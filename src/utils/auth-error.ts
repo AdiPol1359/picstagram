90f79ff4a9000ea6acae110982acbfb1b26aac6e
adipol1359 <aditv1337@gmail.com>
2023-05-04 14:25:33 +0200

    refactor: change variable name

--- src/utils/auth-error.ts
+++ src/utils/auth-error.ts
@@ -1,10 +1,10 @@
 const DEFAULT_ERROR_MESSAGE =
 	'An unexpected error occurred while creating your account!';
 
-const signInPageErrorMessage: Record<string, string> = {
+const signInPageErrorMessages: Record<string, string> = {
 	OAuthAccountNotLinked:
 		'An account with the given e-mail address already exists!',
 };
 
 export const getSignInPageErrorMessage = (error: string) =>
-	signInPageErrorMessage[error] || DEFAULT_ERROR_MESSAGE;
+	signInPageErrorMessages[error] || DEFAULT_ERROR_MESSAGE;
