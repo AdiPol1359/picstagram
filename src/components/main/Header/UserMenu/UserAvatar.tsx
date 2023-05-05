68f54d460bce34468bd60c4c84678b50430159ad
Adrian Polak <aditv1337@gmail.com>
2023-05-05 09:42:50 +0200

    feat: add user menu (#8)

---
+++ src/components/main/Header/UserMenu/UserAvatar.tsx
@@ -0,0 +1,18 @@
+import { useSession } from 'next-auth/react';
+
+import { Avatar } from '@/components/ui/Avatar/Avatar';
+import { getFirstLetter } from '@/utils/string';
+
+export const UserAvatar = () => {
+	const { data } = useSession();
+
+	if (!data?.user?.name) return null;
+
+	const { name, image } = data.user;
+
+	if (image) {
+		return <Avatar src={image} alt={name} />;
+	}
+
+	return <Avatar>{getFirstLetter(name)}</Avatar>;
+};
