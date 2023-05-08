c035d5828469130e28a40b7b71aeb099635f2b50
adipol1359 <aditv1337@gmail.com>
2023-05-08 13:41:36 +0200

    refactor: remove console.log

--- src/components/main/Header/UserMenu/UserAvatar.tsx
+++ src/components/main/Header/UserMenu/UserAvatar.tsx
@@ -10,8 +10,6 @@ export const UserAvatar = () => {
 
 	const { name, image } = data.user;
 
-	console.log(data.user.id, data.user.username);
-
 	if (image) {
 		return <Avatar src={image} alt={name} />;
 	}
