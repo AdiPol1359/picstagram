7ab71496881544309248b37279ed2fe3498febbe
adipol1359 <aditv1337@gmail.com>
2023-05-08 16:50:42 +0200

    feat: add required props to rhf

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -3,16 +3,28 @@ import { FaCheck } from 'react-icons/fa';
 
 import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
 
+import type { ChangeEventHandler, FocusEventHandler } from 'react';
+
 type CheckboxProps = Readonly<{
 	error?: string;
+	name?: string;
+	onChange?: ChangeEventHandler<HTMLInputElement>;
+	onBlur?: FocusEventHandler<HTMLInputElement>;
 	label: string;
 }>;
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-	({ error, label }, ref) => (
+	({ error, name, onChange, onBlur, label }, ref) => (
 		<div className="space-y-1">
 			<label className="flex w-fit cursor-pointer items-center gap-x-1.5 text-sm font-medium">
-				<input type="checkbox" ref={ref} className="peer hidden" />
+				<input
+					type="checkbox"
+					ref={ref}
+					name={name}
+					onChange={onChange}
+					onBlur={onBlur}
+					className="peer hidden"
+				/>
 				<div className="flex h-6 w-6 items-center justify-center rounded-md border text-xs text-white transition-colors duration-100 peer-checked:border-primary-400 peer-checked:bg-primary">
 					<FaCheck />
 				</div>
