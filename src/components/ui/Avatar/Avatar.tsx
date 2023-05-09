1be355d3dcefd776be6da22c384c6c55dd9ed4a0
adipol1359 <aditv1337@gmail.com>
2023-05-09 18:08:45 +0200

    feat: add sizes to avatar

--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -3,19 +3,30 @@ import { twMerge } from 'tailwind-merge';
 
 import type { ReactNode } from 'react';
 
+const sizes = {
+	xs: 'h-8 w-8',
+	sm: 'h-11 w-11',
+	md: 'h-16 w-16',
+	lg: 'h-28 w-28',
+	xl: 'h-36 w-36',
+} as const;
+
 type AvatarProps = Readonly<
-	| {
-			src: string;
-			alt: string;
-			children?: undefined;
-	  }
-	| { children: ReactNode; src?: undefined; alt?: undefined }
+	{ size?: keyof typeof sizes } & (
+		| {
+				src: string;
+				alt: string;
+				children?: undefined;
+		  }
+		| { children: ReactNode; src?: undefined; alt?: undefined }
+	)
 >;
 
-export const Avatar = ({ src, alt, children }: AvatarProps) => (
+export const Avatar = ({ size = 'sm', src, alt, children }: AvatarProps) => (
 	<div
 		className={twMerge(
-			'relative h-11 w-11 overflow-hidden rounded-full',
+			'relative overflow-hidden rounded-full',
+			sizes[size],
 			children &&
 				'flex items-center justify-center bg-primary-400 font-medium text-white'
 		)}
