7538b055fd855bd097bc98687e72bc4f334b61fe
adipol1359 <aditv1337@gmail.com>
2023-05-12 20:38:57 +0200

    refactor: change statistic name

--- src/components/main/ProfileHeader/ProfileStatistics/ProfileStatistics.tsx
+++ src/components/main/ProfileHeader/ProfileStatistics/ProfileStatistics.tsx
@@ -2,7 +2,7 @@ import { StatisticItem } from './StatisticItem';
 
 export const ProfileStatistics = () => (
 	<ul className="flex justify-between">
-		<StatisticItem name="Posts" value={10} />
+		<StatisticItem name="Photos" value={10} />
 		<StatisticItem name="Followers" value={5} />
 		<StatisticItem name="Following" value={7} />
 	</ul>
