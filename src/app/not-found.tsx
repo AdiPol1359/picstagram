652d2990897b965602d5646f6688997ac0b94120
adipol1359 <aditv1337@gmail.com>
2023-05-17 18:24:59 +0200

    refactor: remove flex styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
 	return (
-		<hgroup className="mt-10 flex flex-col text-center">
+		<hgroup className="mt-10 text-center">
 			<h1 className="text-2xl font-semibold">
 				Sorry, this page is unavailable
 			</h1>
