fca4e37e13ef347b54eaa357462bb5e2f72f6266
Adrian Polak <aditv1337@gmail.com>
2023-05-17 18:26:57 +0200

    feat: add not found page (#20)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+	return (
+		<hgroup className="mt-10 text-center">
+			<h1 className="text-2xl font-semibold">
+				Sorry, this page is unavailable
+			</h1>
+			<p className="mt-7 text-gray-800">
+				The link you clicked may be broken or the page may have been removed.
+			</p>
+		</hgroup>
+	);
+}
