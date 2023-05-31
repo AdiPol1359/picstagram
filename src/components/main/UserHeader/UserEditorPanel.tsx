69f80b765bc8466dc047e0a2d5856bb442b8cbc8
adipol1359 <aditv1337@gmail.com>
2023-05-31 16:45:05 +0200

    refactor: change tag name

--- src/components/main/UserHeader/UserEditorPanel.tsx
+++ src/components/main/UserHeader/UserEditorPanel.tsx
@@ -28,9 +28,9 @@ export const UserEditorPanel = ({ user, children }: UserEditorPanelProps) => {
 		<>
 			<div onClick={openModal} className="group relative h-fit w-fit">
 				{children}
-				<div className="absolute left-0 top-0 z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black/60 text-3xl text-white opacity-0 transition-opacity group-hover:opacity-100">
+				<button className="absolute left-0 top-0 z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black/60 text-3xl text-white opacity-0 transition-opacity group-hover:opacity-100">
 					<AiOutlineEdit />
-				</div>
+				</button>
 			</div>
 			<UpdateUserModal user={user} isOpen={isOpen} onClose={closeModal} />
 		</>
