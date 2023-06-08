15bcf74ec39cabc8269eb1814093a2f65ec530b7
Adrian Polak <aditv1337@gmail.com>
2023-06-08 15:44:01 +0200

    feat: add regex to user biography (#48)

--- src/components/main/UpdateUserModal/UpdateUserForm/UpdateUserForm.schemas.ts
+++ src/components/main/UpdateUserModal/UpdateUserForm/UpdateUserForm.schemas.ts
@@ -1,13 +1,17 @@
 import { z } from 'zod';
 
+import { BIOGRAPHY_REGEX } from '@/lib/constants';
 import { simpleUserFormSchema } from '@/lib/schemas';
 
 export const updateUserFormSchema = z
 	.object({
 		biography: z
 			.string()
+			.regex(
+				BIOGRAPHY_REGEX,
+				'Please use up to 40 characters per line and a maximum of 3 lines'
+			)
 			.trim()
-			.nullable()
 			.transform((value) => value || null),
 	})
 	.merge(simpleUserFormSchema);
