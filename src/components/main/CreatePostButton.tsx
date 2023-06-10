7c27e37b1fd0690433cc0619cb24efd7f8a80136
Adrian Polak <aditv1337@gmail.com>
2023-06-10 21:39:03 +0200

    fix: fix button visibility for other users (#54)

--- src/components/main/CreatePostButton.tsx
+++ src/components/main/CreatePostButton.tsx
@@ -1,12 +1,25 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
+
 import { Button } from '../ui/Button/Button';
 import { CreatePostModal } from './CreatePostModal/CreatePostModal';
 
 import { useModal } from '@/hooks/useModal';
 
-export const CreatePostButton = () => {
+import type { User } from '@/server/modules/users/users.schemas';
+
+type CreatePostButtonProps = Readonly<{
+	user: User;
+}>;
+
+export const CreatePostButton = ({ user }: CreatePostButtonProps) => {
+	const { data } = useSession();
 	const { isOpen, openModal, closeModal } = useModal();
+
+	if (data?.user.id !== user.id) {
+		return null;
+	}
 
 	return (
 		<>
