cf3f4810702987ff2dbdea2a6bc4a247aceecff5
Adrian Polak <aditv1337@gmail.com>
2023-06-11 11:24:21 +0200

    refactor: refactor post statistics (#55)

--- src/components/main/UserPosts/PostList/SinglePost/SinglePost.tsx
+++ src/components/main/UserPosts/PostList/SinglePost/SinglePost.tsx
@@ -2,18 +2,9 @@ import Image from 'next/image';
 import { AiFillHeart, AiFillMessage } from 'react-icons/ai';
 import { TbLayersSubtract } from 'react-icons/tb';
 
-import { formatNumber } from '@/lib/utils/intl';
-
-import type { ReactNode } from 'react';
+import { StatisticItem } from './StatisticItem';
 
 import type { Post } from '@/server/modules/posts/posts.schemas';
-
-const Statistic = ({ icon, value }: { icon: ReactNode; value: number }) => (
-	<div className="flex items-center gap-x-1.5 text-xl">
-		{icon}
-		{formatNumber(value)}
-	</div>
-);
 
 type SinglePostProps = Readonly<{
 	post: Post;
@@ -27,9 +18,9 @@ export const SinglePost = ({
 		{images.length > 1 && (
 			<TbLayersSubtract className="absolute right-3 top-2.5 text-xl text-white" />
 		)}
-		<div className="absolute left-0 top-0 flex h-full w-full items-center justify-center gap-x-5 bg-black/50 text-white opacity-0 transition-opacity duration-150 group-hover:opacity-100">
-			<Statistic icon={<AiFillHeart />} value={0} />
-			<Statistic icon={<AiFillMessage />} value={0} />
-		</div>
+		<ul className="absolute left-0 top-0 flex h-full w-full items-center justify-center gap-x-7 bg-black/50 text-white opacity-0 transition-opacity duration-150 group-hover:opacity-100">
+			<StatisticItem icon={<AiFillHeart />} value={0} />
+			<StatisticItem icon={<AiFillMessage />} value={0} />
+		</ul>
 	</article>
 );
