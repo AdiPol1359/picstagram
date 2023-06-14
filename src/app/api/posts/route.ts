13ad109229d82a83247d19f0732c5415fb25b5bb
Adrian Polak <aditv1337@gmail.com>
2023-06-14 09:12:33 +0200

    refactor: refactor payload validation (#60)

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,19 +1,11 @@
 import { NextResponse } from 'next/server';
 import crypto from 'node:crypto';
 
-import { POST_DESCRIPTION_MAX_LENGTH } from '@/lib/constants';
 import { createImage } from '@/lib/services/cloudinary.service';
 import { createJsonResponse, protectRoute } from '@/lib/utils/route';
 import { mapPrismaPostToPost } from '@/server/modules/posts/posts.mapper';
+import { createPostSchema } from '@/server/modules/posts/posts.schemas';
 import { createPost } from '@/server/modules/posts/posts.service';
-
-const validateImages = (data: unknown): data is File[] =>
-	Array.isArray(data) &&
-	data.length > 0 &&
-	data.every((item) => item instanceof Blob);
-
-const validateDescription = (data: unknown): data is string =>
-	typeof data === 'string' && data.length <= POST_DESCRIPTION_MAX_LENGTH;
 
 export const POST = async (request: Request) => {
 	const [err, session] = await protectRoute();
@@ -21,13 +13,16 @@ export const POST = async (request: Request) => {
 	if (err) return err;
 
 	const data = await request.formData();
+	const result = await createPostSchema.safeParseAsync({
+		description: data.get('description'),
+		images: data.getAll('images'),
+	});
 
-	const images = data.getAll('images');
-	const description = data.get('description');
-
-	if (!validateImages(images) || !validateDescription(description)) {
+	if (!result.success) {
 		return createJsonResponse({ message: 'Invalid payload!' }, 400);
 	}
+
+	const { description, images } = result.data;
 
 	const urls = await Promise.all(
 		images.map((image) => createImage(image, { publicId: crypto.randomUUID() }))
